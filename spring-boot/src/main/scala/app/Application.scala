c646d00e780f09af32bc110c238f5018704e1dd8
Leonid Turnaev <lmars@mail.ru>
2016-08-04 13:35:58 +0700

    Упростил Application
    - можно и вот так

--- spring-boot/src/main/scala/app/Application.scala
+++ spring-boot/src/main/scala/app/Application.scala
@@ -1,8 +1,12 @@
 package app
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application
 
 object Application extends App {
 
-  SpringApplication.run(classOf[Config])
+  SpringApplication.run(classOf[Application])
 
 }
--- spring-boot/src/main/scala/app/Config.scala
+++
@@ -1,9 +0,0 @@
-package app
-
-import org.springframework.context.annotation.{ ComponentScan, Configuration }
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
-class Config
