f214f2c2d03fcadd0974ed994264a7e1f3170bc2
wedens <wedens@eldis.ru>
2016-08-05 10:44:45 +0700

    add TableQuery objects, set up database on startup

--- native/src/main/scala/app/Application.scala
+++ native/src/main/scala/app/Application.scala
@@ -11,6 +11,7 @@ import spray.http.MediaTypes._
 import upickle.default._
 import scala.concurrent.duration._
 import com.typesafe.config._
+import slick.driver.H2Driver.api._
 
 class HttpServiceActor extends Actor with HttpService {
 
@@ -29,9 +30,17 @@ object Application extends App {
   implicit val system = ActorSystem("on-spray-can")
   implicit val timeout = Timeout(5.seconds)
 
+  val db = Database.forConfig("h2mem1")
+
+  val setup = (
+    tables.customers.schema ++
+    tables.orders.schema ++
+    tables.products.schema
+  ).create
+
+  db.run(setup)
+
   val httpConfig = ConfigFactory.load.getConfig("http")
-
   val service = system.actorOf(Props[HttpServiceActor], "my-service")
-
   IO(Http) ? Http.Bind(service, interface = httpConfig.getString("interface"), port = httpConfig.getInt("port"))
 }
--- native/src/main/scala/app/Db.scala
+++ native/src/main/scala/app/Db.scala
@@ -2,15 +2,33 @@ package app
 
 import slick.driver.H2Driver.api._
 
-object db {
+object tables {
 
-  class CustomersTable(tag: Tag) extends Table[Customer](tag, "customers") {
+  class Customers(tag: Tag) extends Table[Customer](tag, "customers") {
     def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
     def name = column[String]("name")
 
     def * = (id.?, name) <> (Customer.tupled, Customer.unapply)
   }
 
-  val customers = TableQuery[CustomersTable]
+  val customers = TableQuery[Customers]
 
+  class Orders(tag: Tag) extends Table[Order](tag, "orders") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def customerId = column[Long]("customer_id")
+    def customer = foreignKey("customers", customerId, customers)(_.id)
+
+    def * = (id.?, customerId) <> (Order.tupled, Order.unapply)
+  }
+
+  val orders = TableQuery[Orders]
+
+  class Products(tag: Tag) extends Table[Product](tag, "products") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def name = column[String]("name")
+
+    def * = (id.?, name) <> (Product.tupled, Product.unapply)
+  }
+
+  val products = TableQuery[Products]
 }
